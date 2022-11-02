94a7fd630514eb29f9101f92589fab87061c1f39
tanykazy <tanykazy@gmail.com>
2022-11-03 00:00:10 +0900

    Use storage

--- background.js
+++ background.js
@@ -17,3 +17,12 @@ chrome.runtime.onConnect.addListener(function (port) {
     // chrome.action.openPopup();
   });
 });
+
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+  for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+    console.log(
+      `Storage key "${key}" in namespace "${namespace}" changed.`,
+      `Old value was "${oldValue}", new value is "${newValue}".`
+    );
+  }
+});
--- popup.js
+++ popup.js
@@ -1,14 +1,33 @@
-const textarea = window.document.getElementById('editor');
-
 console.log(CodeMirror.version);
+
+CodeMirror.commands.save = function (editor) {
+  chrome.storage.local.set({
+    value: editor.doc.getValue(),
+  }, function () {
+    console.log('Saving');
+  });
+};
+
+const textarea = window.document.getElementById('editor');
 const editor = CodeMirror.fromTextArea(textarea, {
+  matchBrackets: true,
+  showCursorWhenSelecting: true,
+  inputStyle: "contenteditable",
   theme: 'solarized dark',
   keyMap: 'vim',
   lineWrapping: true,
   lineNumbers: true,
   spellcheck: true,
-  autocorrect: true
+  autocorrect: true,
+});
+editor.on('change', function (instance, changeObj) {
+  // console.log(instance);
+  // console.log(changeObj);
 });
 editor.setSize('100%', '100%');
 editor.refresh();
 editor.focus();
+
+chrome.storage.local.get('value', function (result) {
+  editor.doc.setValue(result.value);
+});
