2627cbafc714a9cd4d68235f5445ba7a36c8ffe6
tanykazy <tanykazy@gmail.com>
2022-11-08 22:05:40 +0900

    Use monaco editor

--- content-script.js
+++ content-script.js
@@ -1,26 +1,26 @@
 function onClicked(event) {
-    CodeMirror.commands.save = function (editor) {
-        editor.save();
-    };
+    // CodeMirror.commands.save = function (editor) {
+    //     editor.save();
+    // };
 
-    CodeMirror.Vim.defineEx('quit', 'q', function (editor, params) {
-        editor.toTextArea();
-    });
+    // CodeMirror.Vim.defineEx('quit', 'q', function (editor, params) {
+    //     editor.toTextArea();
+    // });
 
-    const editor = CodeMirror.fromTextArea(event.target, {
-        matchBrackets: true,
-        showCursorWhenSelecting: true,
-        inputStyle: "contenteditable",
-        keyMap: 'vim',
-        lineWrapping: true,
-        lineNumbers: true,
-        spellcheck: true,
-        autocorrect: true,
-    });
+    // const editor = CodeMirror.fromTextArea(event.target, {
+    //     matchBrackets: true,
+    //     showCursorWhenSelecting: true,
+    //     inputStyle: "contenteditable",
+    //     keyMap: 'vim',
+    //     lineWrapping: true,
+    //     lineNumbers: true,
+    //     spellcheck: true,
+    //     autocorrect: true,
+    // });
 
-    editor.setSize('100%', '100%');
-    editor.refresh();
-    editor.focus();
+    // editor.setSize('100%', '100%');
+    // editor.refresh();
+    // editor.focus();
 }
 
 const textarea = window.document.getElementsByTagName('textarea');
--- popup.js
+++ popup.js
@@ -1,27 +1,33 @@
-CodeMirror.commands.save = function (editor) {
-  chrome.storage.local.set({
-    value: editor.doc.getValue(),
-  }, function () {
-    console.log('Saving');
-  });
-};
+var require = { paths: { vs: 'third-party/node_modules/monaco-editor/min/vs' } };
 
-const editor = CodeMirror(window.document.getElementById('editor'), {
-  matchBrackets: true,
-  showCursorWhenSelecting: true,
-  scrollbarStyle: 'null',
-  inputStyle: "contenteditable",
-  theme: 'solarized dark',
-  keyMap: 'vim',
-  lineWrapping: true,
-  lineNumbers: true,
-  spellcheck: true,
-  autocorrect: true,
+var editor = monaco.editor.create(document.getElementById('container'), {
+  value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
+  language: 'javascript'
 });
-editor.setSize('100%', '100%');
-editor.refresh();
-editor.focus();
+// CodeMirror.commands.save = function (editor) {
+//   chrome.storage.local.set({
+//     value: editor.doc.getValue(),
+//   }, function () {
+//     console.log('Saving');
+//   });
+// };
 
-chrome.storage.local.get('value', function (items) {
-  editor.doc.setValue(items.value);
-});
+// const editor = CodeMirror(window.document.getElementById('editor'), {
+//   matchBrackets: true,
+//   showCursorWhenSelecting: true,
+//   scrollbarStyle: 'null',
+//   inputStyle: "contenteditable",
+//   theme: 'solarized dark',
+//   keyMap: 'vim',
+//   lineWrapping: true,
+//   lineNumbers: true,
+//   spellcheck: true,
+//   autocorrect: true,
+// });
+// editor.setSize('100%', '100%');
+// editor.refresh();
+// editor.focus();
+
+// chrome.storage.local.get('value', function (items) {
+//   editor.doc.setValue(items.value);
+// });
